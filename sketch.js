2b9b69bdf4eaad47f6186c809b68d3d4742fe28a
icegamer910 <101666859+icegamer910@users.noreply.github.com>
2022-09-13 15:53:59 -0300

    13/9

--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
-var galpao
-var sobrevivente
-var sobreviventeimg
-
-
+var galpao;
+var sobrevivente;
+var sobreviventeimg;
+var sobreviventeemperigo;
+var fire;
+var fireimg;
 
 function preload(){
 galpao = loadImage("./assets/bg.jpeg");  
 sobreviventeimg = loadImage("./assets/shooter_2.png");
+sobreviventeemperigo = loadImage("./assets/shooter_3.png");
+fireimg = loadImage("./assets/fire.png");
 }
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
 sobrevivente = createSprite(width/2,600);
-sobrevivente.addImage(sobreviventeimg);
-sobrevivente.scale = 0.8
-}
+sobrevivente.addImage("restante",sobreviventeimg);
+sobrevivente.addImage("altodefesa",sobreviventeemperigo)
+sobrevivente.scale = 0.8;
+fire = createSprite(width-100,170);
+fire.addImage(fireimg);
+fire.scale = 0.09
+};
 
 function draw() {
 background(galpao);
@@ -30,6 +37,15 @@ sobrevivente.x+=10;
  if(keyDown("a")){
  sobrevivente.x-=10;
 }
+if(mousePressedOver(fire)){
+sobrevivente.changeImage("altodefesa")
+sobrevivente.scale = 1
+}
+else{
+sobrevivente.changeImage("restante")
+sobrevivente.scale = 0.8
+}
+
 
 drawSprites()
 }
